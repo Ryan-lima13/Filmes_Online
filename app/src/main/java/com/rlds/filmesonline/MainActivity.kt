03071c452f27ff3ec1c887bb90985fdde15ab2d5
ryanl <ryanlimadev@gmail.com>
2022-09-08 22:52:15 -0300

    criando tela principal

--- app/src/main/java/com/rlds/filmesonline/MainActivity.kt
+++ app/src/main/java/com/rlds/filmesonline/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.rlds.filmesonline
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+           abirTelaLogin()
+
+        },2000)
+    }
+    private fun abirTelaLogin(){
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
