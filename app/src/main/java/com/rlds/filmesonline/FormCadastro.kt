62ef32a34b63ff0c9cda83057ac51b55e058da5e
ryanl <ryanlimadev@gmail.com>
2022-09-08 23:10:49 -0300

    criando tela de cadastro

--- app/src/main/java/com/rlds/filmesonline/FormCadastro.kt
+++ app/src/main/java/com/rlds/filmesonline/FormCadastro.kt
@@ -7,5 +7,6 @@ class FormCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
+        supportActionBar!!.hide()
     }
 }
