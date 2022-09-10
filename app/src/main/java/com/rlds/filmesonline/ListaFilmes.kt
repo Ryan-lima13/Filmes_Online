c90ca959e844d814087c853e801eda45d960eae0
ryanl <ryanlimadev@gmail.com>
2022-09-09 21:47:43 -0300

    deslogando usuario

--- app/src/main/java/com/rlds/filmesonline/ListaFilmes.kt
+++ app/src/main/java/com/rlds/filmesonline/ListaFilmes.kt
@@ -1,11 +1,43 @@
 package com.rlds.filmesonline
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.rlds.filmesonline.databinding.ActivityListaFilmesBinding
 
 class ListaFilmes : AppCompatActivity() {
+    private lateinit var binding:ActivityListaFilmesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_filmes)
+        binding = ActivityListaFilmesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.menu,menu)
+        return  true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_sair ->{
+                FirebaseAuth.getInstance().signOut()
+                voltarTelaTelaLogin()
+
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun voltarTelaTelaLogin(){
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
